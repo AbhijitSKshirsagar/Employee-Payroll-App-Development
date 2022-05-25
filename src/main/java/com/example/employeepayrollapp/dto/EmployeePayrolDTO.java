807543c7b0c5f9854cfcef87b29001d26a793d66
AbhijitSKshirsagar <abhijitkshirsagar793@gmail.com>
2022-05-25 14:48:54 +0530

    added: Ensure validation is done on the payroll DTO

--- src/main/java/com/example/employeepayrollapp/dto/EmployeePayrolDTO.java
+++ src/main/java/com/example/employeepayrollapp/dto/EmployeePayrolDTO.java
@@ -1,23 +1,29 @@
 package com.example.employeepayrollapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import lombok.ToString;
 
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.util.List;
 
 @Data
 public class EmployeePayrolDTO {
 
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message ="Employee firstname is invalid")
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message ="Employee firstname pattern is wrong")
     public String fName;
-    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",message ="Employee lastname is invalid")
-    public String lName;
-    @NotEmpty(message = "Company name should not be empty")
-    public String company;
-    public String roll;
     @Min(value = 500, message = "Min salary should be more than 500")
     public int salary;
+    @Pattern(regexp = "male|female",message = "Gender needs to be  male or female")
+    public String gender;
+    @JsonFormat(pattern = "dd MMM yyyy")
+    @NotNull(message = "startdate should not be empty")
+    public LocalDate startDate;
+    @NotBlank(message = "Note cannot be empty")
+    public String note;
+    @NotBlank(message = "profilePic cannot be empty")
+    public String profilePic;
+    @NotNull(message = "department should not be empty")
+    public List<String> department;
 
 }
--- src/main/java/com/example/employeepayrollapp/entity/EmployeePayrolData.java
+++ src/main/java/com/example/employeepayrollapp/entity/EmployeePayrolData.java
@@ -2,25 +2,30 @@ package com.example.employeepayrollapp.entity;
 
 import com.example.employeepayrollapp.dto.EmployeePayrolDTO;
 import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+
 @Data
 public class EmployeePayrolData {
     private int id;
     private String fName;
-    private String lName;
-    private String company;
-    private String roll;
     private int salary;
-
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private  String profilePic;
+    private List<String> department;
     public EmployeePayrolData() {
     }
-
     public EmployeePayrolData(int id, EmployeePayrolDTO employeePayrolDTO) {
         this.id = id;
-        this.fName = employeePayrolDTO.getFName();
-        this.lName = employeePayrolDTO.getLName();
-        this.company = employeePayrolDTO.getCompany();
-        this.roll = employeePayrolDTO.getRoll();
+        this.fName=employeePayrolDTO.getFName();
         this.salary = employeePayrolDTO.getSalary();
+        this.gender=employeePayrolDTO.getGender();
+        this.startDate = employeePayrolDTO.getStartDate();
+        this.note = employeePayrolDTO.getNote();
+        this.profilePic=employeePayrolDTO.getProfilePic();
+        this.department=employeePayrolDTO.getDepartment();
     }
-
 }
